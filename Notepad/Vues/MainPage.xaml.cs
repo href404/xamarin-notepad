1859a192f72b4435b6fb9979d3ca946df2724acb
Pierre Julien <p.julien@intech-sud.fr>
2020-03-20 16:43:04 +0100

    Modification du nom de la signature de l'evenement permettant la navigation vers la page parametres

--- Notepad/Vues/MainPage.xaml.cs
+++ Notepad/Vues/MainPage.xaml.cs
@@ -31,17 +31,19 @@ namespace Notepad.Vues
 
         #region Evenements
 
-        private void SurAppuiSauvegarder(object sender, EventArgs e) { 
+        private void SurAppuiSauvegarder(object sender, EventArgs e) 
+        { 
             GestionnaireNote.SauvegarderNote();
             DisplayAlert("Sauvgarde", "Votre note a bien été enregistrée", "OK");
         }
 
-        private async void SurAppuiSupprimer(object sender, EventArgs e) {
+        private async void SurAppuiSupprimer(object sender, EventArgs e) 
+        {
             if (await DisplayAlert("Suppression", "Êtes-vous sur de vouloir supprimer votre note ?", "Oui", "Non"))
                 GestionnaireNote.SupprimerNote(); 
         }
 
-        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        private async void SurAppuiParametres(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new ParametrePage(GestionnaireParametre));
         }
