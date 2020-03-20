7f3d6da56e37b232cd011d3a4d54fafdb48803d3
Pierre Julien <p.julien@intech-sud.fr>
2020-03-20 11:31:55 +0100

    Ajout d'un gestionnaire permettant la sauvegarde et la recuperation des notes

--- Notepad/Vues/MainPage.xaml.cs
+++ Notepad/Vues/MainPage.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,31 +7,16 @@ namespace Notepad.Vues
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
-        public MainPage()
-        {
+        private readonly GestionnaireNotes GestionnaireNotes;
+
+        public MainPage() { 
             InitializeComponent();
+
+            GestionnaireNotes = new GestionnaireNotes(ZoneTextuel);
+            GestionnaireNotes.ChargerNote();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            SauvegarderNote();
-        }
+        private void Button_Clicked(object sender, EventArgs e) { GestionnaireNotes.SauvegarderNote(); }
 
-        private void SauvegarderNote()
-        {
-            try
-            {
-                string cheminFichierNote = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notepad.txt");
-                string note = ZoneTextuel.Text;
-
-                File.AppendAllText(cheminFichierNote, note);
-                Debug.WriteLine("Sauvegarde effectuée !");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                ZoneTextuel.Text = ex.Message;
-            }
-        }
     }
 }
