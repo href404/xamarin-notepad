6b90e39126969726f5b41c0c03c216b530cfc0b5
Pierre Julien <p.julien@intech-sud.fr>
2020-03-20 11:20:24 +0100

    Création de l'application Xamarin permettant une prise de note

---
+++ Notepad/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Notepad.Vues;
+using Xamarin.Forms;
+
+namespace Notepad
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart() { }
+
+        protected override void OnSleep() { }
+
+        protected override void OnResume() { }
+    }
+}
---
+++ Notepad/Vues/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notepad.Vues
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            SauvegarderNote();
+        }
+
+        private void SauvegarderNote()
+        {
+            try
+            {
+                string cheminFichierNote = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notepad.txt");
+                string note = ZoneTextuel.Text;
+
+                File.AppendAllText(cheminFichierNote, note);
+                Debug.WriteLine("Sauvegarde effectuée !");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                ZoneTextuel.Text = ex.Message;
+            }
+        }
+    }
+}
