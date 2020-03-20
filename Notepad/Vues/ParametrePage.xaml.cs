779bd006dfee3c98364accd3dda270422e485008
Pierre Julien <p.julien@intech-sud.fr>
2020-03-20 15:11:07 +0100

    Ajout d'une page permettant le paramètrage de l'application

---
+++ Notepad/Vues/ParametrePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notepad.Vues
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ParametrePage : ContentPage
+    {
+        public ParametrePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Switch_Toggled_1(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
