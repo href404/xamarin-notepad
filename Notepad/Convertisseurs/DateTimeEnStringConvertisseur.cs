00bcac94563646fe1e89ff6a60a1ff6d8ba35dcd
Pierre Julien <pierrejulienpro@outlook.fr>
2020-03-27 11:59:10 +0100

    Ajout d'un convertisseur pour afficher la date en chaine de caractere dans un format specifique dans la liste des notes

---
+++ Notepad/Convertisseurs/DateTimeEnStringConvertisseur.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Notepad.Convertisseurs
+{
+    public class DateTimeEnStringConvertisseur : IValueConverter
+    {
+        #region Constantes
+
+        private const string FORMAT_DATE = "f";
+
+        #endregion
+
+        #region Implémentation IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is DateTime))
+                return string.Empty;
+
+            DateTime date = (DateTime) value;
+            return date.ToString(FORMAT_DATE);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        #endregion
+
+    }
+}
